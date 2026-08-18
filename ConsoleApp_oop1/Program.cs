a88c157ff2ae796eec59fdafcd8ee015068e6d2d
hamza-salah12 <privatew828@gmail.com>
2026-08-18 17:49:02 +0300

    Theoretical Questions

--- ConsoleApp_oop1/Program.cs
+++ ConsoleApp_oop1/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Theoretical Questions
+            //Part 01 : Theoretical Questions
+            //Question 1 : in case 1 : the data is copied completely therefor modifying the new var  don't effected about new var   
+            //------------------------
+            //Question 2 :  in case 2 : to copy is address therefor the two var look at the same object in the heap therefor the original effected by change 
+
+            #endregion
+
         }
     }
 }
